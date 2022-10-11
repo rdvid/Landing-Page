c08f59a1bf896f139a5433eca5fdb81679dbdda0
Rafael D <rafael_dvid@hotmail.com>
2022-10-11 20:23:35 -0300

    fixing scroll bug and add card to cta section

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ $(document).ready(function () {
                         Entregadores para realizar entregas
                         avulsas de acordo com a disponibilidade
                         de parceiros próximos.
+
+                        
                         Sem custo fixo, somente as taxas de
                         entrega disponibilizadas pela plataforma
                     </p>
@@ -52,5 +54,22 @@ $(document).ready(function () {
                 </li>
             </ul>
         </div>
+
+
+
+<h2>Curtiu?</h2>
+                <p>Cadastre-se no botão abaixo para solicitar a visita de um consultor:</p>
+                <button class="btn btn-default-sm btn-contato">
+                    <a href="https://l.wl.co/l?u=https%3A%2F%2Facesso.pickngo.online%2FEmpresa%2FCadastrar%3Fcodigo%3DZERODOZE"
+                        target="_blank">
+                        Cadastrar </a>
+                </button>
+
+
+
+
+
+
+
 
 */
