cacc8679ade88c08aaa2eb0e6c6c8317f80655a8
Rafael D <rafael_dvid@hotmail.com>
2022-10-11 00:07:21 -0300

    add new planos section with responsive card

--- script.js
+++ script.js
@@ -6,7 +6,51 @@ $(document).ready(function () {
         $(window).scroll('href')
     });
 
-
-
-
 });
+
+/*
+
+
+            <h2>Escolha a melhor opção para o seu negócio
+            </h2>
+            <ul>
+                <li>
+                    <h3>Entregadores por Disponibilidade:
+                    </h3>
+                    <p>
+                        Entregadores para realizar entregas
+                        avulsas de acordo com a disponibilidade
+                        de parceiros próximos.
+                        Sem custo fixo, somente as taxas de
+                        entrega disponibilizadas pela plataforma
+                    </p>
+                </li>
+                <li>
+                    <h3>Entregas Premium:</h3>
+                    <p>
+                        Entregadores para realizar entregas entre
+                        empresas próximas, de acordo com a
+                        disponibilidade de parceiros na região.
+                        Entregadores disponíveis na tela
+                        atribuição, tempo de espera 5 minutos por
+                        km de deslocamento.
+
+                        Sem custo fixo, somente as taxas de entrega
+                        disponibilizadas pela plataforma.
+                    </p>
+                </li>
+                <li>
+                    <h3>Entregas Exclusivas:</h3>
+                    <p>
+                        Entregadores para realizar entregas
+                        somente para seu estabelecimento.
+
+                        Planos a partir de R$ 30,00 (trinta reais) por
+                        período + taxas de entrega disponibilizadas pela
+                        plataforma.
+                    </p>
+                </li>
+            </ul>
+        </div>
+
+*/
